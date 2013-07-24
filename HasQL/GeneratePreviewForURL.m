a9fc92ea64b3c0a0de13bee6cd012313d730dff6
David Dreshfield <david@dreshfield.com>
2013-07-23 22:34:29 -0700

    First attempt at implementing generator code; minor correction to bundle versioning schema in Info.plist.

--- HasQL/GeneratePreviewForURL.m
+++ HasQL/GeneratePreviewForURL.m
@@ -1,6 +1,7 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
+#import <Cocoa/Cocoa.h>
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
@@ -13,8 +14,33 @@ void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
-    // To complete your generator please implement the function GeneratePreviewForURL in GeneratePreviewForURL.c
+    @autoreleasepool {
+        NSError *error = nil;
+        NSURL *hs2URL = (__bridge NSURL *)url;
+        
+        // Load document data
+        NSStringEncoding hsEncoding;
+        NSString *hsString = [NSString stringWithContentsOfURL:hs2URL usedEncoding:&hsEncoding error:&error];
+        // Try ISO-8859-1 if encoding can't be determined
+        if (!hsString) {
+            hsEncoding = NSISOLatin1StringEncoding;
+            hsString = [NSString stringWithContentsOfURL:hs2URL encoding:hsEncoding error:&error];
+            // Throw error if fallback fails
+            if (!hsString) {
+                if (nil !=error) {
+                    NSLog(@"Error opening %@", error);
+                }
+                return noErr;
+            }
+        }
+    
+        if (false == QLPreviewRequestIsCancelled(preview)) {
+            // NSBundle *hsBundle = [NSBundle bundleWithIdentifier:hsString];
+            CFDictionaryRef properties = (__bridge CFDictionaryRef)@{};
+            QLPreviewRequestSetDataRepresentation(preview, (__bridge CFDataRef)[hsString dataUsingEncoding:hsEncoding], kUTTypeSourceCode, properties);
+        }
     return noErr;
+    }
 }
 
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview)
